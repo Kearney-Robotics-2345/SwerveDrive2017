4097645ee3eca33c388ad8866de3fbb893e71aff
KearneyRobotics2345 <kearneyrobotics@outlook.com>
2017-01-17 18:57:10 -0600

    b

--- src/org/usfirst/frc/team2345/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team2345/robot/commands/Autonomous.java
@@ -2,6 +2,18 @@ package org.usfirst.frc.team2345.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+import org.usfirst.frc.team2345.robot.Robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.CANTalon;
+
+import org.usfirst.frc.team2345.robot.RobotMap;
 /**
  *
  */
@@ -14,6 +26,25 @@ public class Autonomous extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    ////TEST CODE
+    	RobotMap.frontLeftTurn.set(0);
+    	RobotMap.frontRightTurn.set(0);
+    	RobotMap.backLeftTurn.set(0);
+    	RobotMap.backRightTurn.set(0);
+    	Timer.delay(2.75);
+    	
+    	//Move Back
+    	RobotMap.frontLeftDrive.set(-.5);
+    	RobotMap.frontRightDrive.set(-.5);
+    	RobotMap.backLeftDrive.set(-.5);
+    	RobotMap.backRightDrive.set(-.5);    	
+    	Timer.delay(2.75); 
+    	
+    	//Stop
+    	RobotMap.frontLeftDrive.set(0);
+    	RobotMap.frontRightDrive.set(0);
+    	RobotMap.backLeftDrive.set(0);
+    	RobotMap.backRightDrive.set(0);
     }
 
     // Called repeatedly when this Command is scheduled to run
